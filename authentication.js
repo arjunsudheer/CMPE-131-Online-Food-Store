85ebb59b158454eb32c1ac5962ddee519d351cc2
arjunsudheer <arjunsudheer05@outlook.com>
2024-03-08 20:03:59 -0800

    Implement customer registration and login front-end
    Add fields according and links according to customer registration and login diagrams

---
+++ authentication.js
@@ -0,0 +1,11 @@
+// Store commonly used elements
+let passwordView = document.getElementById("password-view");
+let passwordInput = document.getElementById("password-input");
+// Show/Hides the password when the toggle is clicked
+passwordView.addEventListener("click", function() {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+});
