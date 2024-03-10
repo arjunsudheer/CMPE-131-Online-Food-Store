d297defbf812dea238e05272d70c8cca6c58dcc5
arjunsudheer <arjunsudheer05@outlook.com>
2024-03-10 14:31:05 -0700

    Split navbar into search bar and left side bar
    Implement cart menu that shows when cart icon is clicked

--- navbar.js
+++ navbar.js
@@ -1,30 +1,28 @@
-let locationList = document.getElementById("location-list");
-let accountList = document.getElementById("account-list");
-// show and hide the location dropdown menu when clicked
-document.getElementById("location-dropdown").addEventListener("click", function () {
-    // show the location dropdown list
-    locationList.classList.toggle("show-list");
-    // hide the account-dropdown list if it is displayed
-    accountList.classList.remove("show-list");
-});
+// set the width of the searchBar
+let searchBar = document.getElementById("product-search-bar");
+const defaultSearchBarWidth = "85%";
+searchBar.style.width = defaultSearchBarWidth;
 
-// show and hide the location dropdown menu when clicked
-document.getElementById("account-dropdown").addEventListener("click", function () {
-    // show the account dropdown list
-    accountList.classList.toggle("show-list");
-    // hide the location-dropdown list if it is displayed
-    locationList.classList.remove("show-list");
-});
-
-// close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches(".navbar-dropdowns")) {
-        let dropdowns = document.getElementsByClassName("navbar-item-list");
-        // loop through all navbar-list-items and remove the "show-list" class
-        for (let i = 0; i < dropdowns.length; i++) {
-            if (dropdowns[i].classList.contains("show-list")) {
-                dropdowns[i].classList.remove("show-list");
-            }
-        }
+document.getElementById("cart-button").addEventListener("click", function () {
+    // adjust the size of the search bar accordingly
+    if (searchBar.style.width == defaultSearchBarWidth) {
+        searchBar.style.width = "70%";
+    } else {
+        searchBar.style.width = defaultSearchBarWidth;
     }
-}
+    // show the cart menu on the right side
+    document.getElementsByClassName("cart-menu")[0].classList.toggle("show-menu");
+});
+
+let quantityAmount = document.getElementById("quantity-amount");
+document.getElementById("subtract-quantity").addEventListener("click", function () {
+    let currentQuantity = parseInt(quantityAmount.innerHTML);
+    if (currentQuantity - 1 >= 0) {
+        quantityAmount.innerHTML = (currentQuantity - 1);
+    }
+});
+
+document.getElementById("add-quantity").addEventListener("click", function () {
+    let currentQuantity = parseInt(quantityAmount.innerHTML);
+    quantityAmount.innerHTML = (currentQuantity + 1);
+});
