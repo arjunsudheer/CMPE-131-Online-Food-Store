4c2420856e5554c9772d7d40976d7d36d69cc68c
arjunsudheer <arjunsudheer05@outlook.com>
2024-04-11 18:09:50 -0700

    Update database query for product information popup
    Query the databse by both the product name and product brand to get accurate product information

--- popups.php
+++ popups.php
@@ -1,9 +1,9 @@
 <?php
 // handles the XMLHTTPRequest from main-page.js
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $productName = htmlspecialchars($_POST['name']);
+    $productName = json_decode($_POST['name']);
     if (isset($productName) && !empty($productName)) {
-        if ($productName === "add-to-cart") {
+        if ($productName[0] === "add-to-cart") {
             createAddToCartPopup();
         } else {
             createProductInformationPopup($productName);
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 // Pop-up menu to show more information about a product
-function createProductInformationPopup($productPopupName)
+function createProductInformationPopup($productPopup)
 {
     // establish connection to database
     $connection = mysqli_connect("localhost", "root", "", "ofs");
@@ -20,29 +20,31 @@ function createProductInformationPopup($productPopupName)
         echo 'error';
         die("Connection failed: " . mysqli_connect_error());
     }
+    // store the product name and brand in variables
+    $product_name = $productPopup[0];
+    $product_brand = $productPopup[1];
     // get the product with the specified name
-    $sql_get_item = "SELECT * FROM Items WHERE Product = '$productPopupName'";
+    $sql_get_item = "SELECT * FROM Items WHERE Product = '$product_name' AND Brand = '$product_brand'";
     $product_item = mysqli_query($connection, $sql_get_item);
     // display the product items on the webpage
     if ($product_item) {
         $row = mysqli_fetch_assoc($product_item);
         // create a new product item div on the main page
-        $product_brand = $row["Brand"];
         $product_type = $row["Type"];
         $product_price = $row["Price"];
         $product_weight = $row["Weight"];
         $product_quantity = $row["inStock"];
         echo ("
                 <div id=\"product-information-popup-animate\" class=\"main-page-box product-information-popup\">
-                <button id=\"popup-close-button\">Close <i class=\"fa fa-window-close-o\" aria-hidden=\"true\"></i></button>
-                <img src=\"#\" alt=\"product-image\" id=\"product-image\">
-                <div id=\"product-information\">
-                    <p class=\"popup-product-info\">$productPopupName ($product_brand)</p>
-                    <p class=\"popup-product-info\"><span class=\"popup-product-label\">Type:</span> $product_type</p>
-                    <p class=\"popup-product-info\"><span class=\"popup-product-label\">Product Price:</span> $$product_price</p>
-                    <p class=\"popup-product-info\"><span class=\"popup-product-label\">Weight:</span> $product_weight lbs.</p>
-                    <p class=\"popup-product-info\"><span class=\"popup-product-label\">Quantity Remaining:</span> $product_quantity</p>
-                </div>
+                    <button id=\"popup-close-button\">Close <i class=\"fa fa-window-close-o\" aria-hidden=\"true\"></i></button>
+                    <img src=\"#\" alt=\"product-image\" id=\"product-image\">
+                    <div id=\"product-information\">
+                        <p class=\"popup-product-info\">$product_name ($product_brand)</p>
+                        <p class=\"popup-product-info\"><span class=\"popup-product-label\">Type:</span> $product_type</p>
+                        <p class=\"popup-product-info\"><span class=\"popup-product-label\">Product Price:</span> $$product_price</p>
+                        <p class=\"popup-product-info\"><span class=\"popup-product-label\">Weight:</span> $product_weight lbs.</p>
+                        <p class=\"popup-product-info\"><span class=\"popup-product-label\">Quantity Remaining:</span> $product_quantity</p>
+                    </div>
                 </div>
                 ");
     } else {
@@ -50,7 +52,6 @@ function createProductInformationPopup($productPopupName)
     }
     // close the database connection
     mysqli_close($connection);
-
 }
 // Popup to show success message once item is added to cart
 function createAddToCartPopup()
